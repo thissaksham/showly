e49953027fb46af20835274f9ebe4e3e451732db
thissaksham <thissaksham@gmail.com>
2026-05-30 21:18:00 +0530

    Initial setup: fix API keys, AGP migration, gradle toolchain

--- data-remote/src/main/java/com/michaldrabik/data_remote/Config.kt
+++ data-remote/src/main/java/com/michaldrabik/data_remote/Config.kt
@@ -21,7 +21,7 @@ object Config {
   const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
   const val TMDB_API_KEY = BuildConfig.TMDB_API_KEY
 
-  const val OMDB_BASE_URL = "https://private.omdbapi.com/"
+  const val OMDB_BASE_URL = "https://omdbapi.com/"
   const val OMDB_API_KEY = BuildConfig.OMDB_API_KEY
 
   const val AWS_BASE_URL = "https://showly2.s3.eu-west-2.amazonaws.com/"
